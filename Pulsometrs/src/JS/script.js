8db5000cab15def9462ea996d91f5c2fb583e4de
Всеволод <macseb@MacBook-Pro-Vsevolod.local>
2020-05-19 14:52:20 +0400

    tabs added

--- Pulsometrs/src/JS/script.js
+++ Pulsometrs/src/JS/script.js
@@ -15,3 +15,27 @@ document.querySelector('.prev').addEventListener ('click', function () {
 document.querySelector('.next').addEventListener ('click', function () {
     slider.goTo('next');
 });
+
+(function($) {
+$(function() {
+
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_activ)', function() {
+    $(this)
+      .addClass('catalog__tab_activ').siblings().removeClass('catalog__tab_activ')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_activ').eq($(this).index()).addClass('catalog__content_activ');
+});
+
+function toggleSlide (item) {
+    $(item).each(function(i) {
+        $(this).on('click', function (e){
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    })
+}
+toggleSlide ('.catalog-item__link')
+toggleSlide ('.catalog-item__back')
+
+});
+})(jQuery);
